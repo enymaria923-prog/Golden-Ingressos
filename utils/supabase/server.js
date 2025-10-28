aa31f7560378b421fe8056266a4fa369d9f257db
enymaria923-prog <enymaria923@gmail.com>
2025-10-28 20:32:46 -0300

    Update server.js

--- utils/supabase/server.js
+++ utils/supabase/server.js
@@ -1,9 +1,15 @@
+// utils/supabase/server.js
+// ESTE É O CÓDIGO OFICIAL DO TEMPLATE SUPABASE
+
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
 export function createClient() {
   const cookieStore = cookies()
 
+  // Cria o cliente Supabase usando as chaves secretas
+  // que o Vercel injetou automaticamente (process.env)
+  // Este cliente é para Server Components e Server Actions
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
     process.env.SUPABASE_SECRET_KEY,
@@ -16,17 +22,19 @@ export function createClient() {
           try {
             cookieStore.set({ name, value, ...options })
           } catch (error) {
-            // erro
+            // O 'set' foi chamado a partir de um Server Component.
+            // Isto pode ser ignorado se tiver um middleware a atualizar as sessões.
           }
         },
         remove(name, options) {
           try {
             cookieStore.set({ name, '', ...options })
           } catch (error) {
-            // erro
+            // O 'remove' foi chamado a partir de um Server Component.
+            // Isto pode ser ignorado se tiver um middleware a atualizar as sessões.
           }
-        }
-      }
+        },
+      },
     }
   )
 }
