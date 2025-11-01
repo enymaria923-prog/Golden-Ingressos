8ca18c51c17d772bea81238e69380b82f9a44f81
enymaria923-prog <enymaria923@gmail.com>
2025-11-01 20:15:23 -0300

    Update client.js

--- utils/supabase/client.js
+++ utils/supabase/client.js
@@ -1,8 +1,9 @@
-import { createBrowserClient } from '@supabase/ssr'
+// utils/supabase/client.js
+import { createBrowserClient } from '@supabase/ssr';
 
-const supabase = createBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
-
-export { supabase };
+export const createClient = () => {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  );
+};
