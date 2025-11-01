7107d3683fd8404b09067d19188730cf365c1b6c
enymaria923-prog <enymaria923@gmail.com>
2025-11-01 03:24:41 -0300

    Update page.js

--- app/publicar-evento/page.js
+++ app/publicar-evento/page.js
@@ -1,6 +1,6 @@
 'use client';
 import React, { useState } from 'react';
-import { supabase } from '../../../utils/supabase/client';
+import { supabase } from '../../utils/supabase/client';
 import { useRouter } from 'next/navigation';
 import './PublicarEvento.css';
 
