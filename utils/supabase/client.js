4fbb0ce810ba33d95aeaa461288c1447697ec0a9
enymaria923-prog <enymaria923@gmail.com>
2025-11-03 16:16:58 -0300

    Update client.js

--- utils/supabase/client.js
+++ utils/supabase/client.js
@@ -2,9 +2,7 @@
 
 import { createBrowserClient } from '@supabase/ssr';
 
-const supabase = createBrowserClient(
+export const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
-
-export { supabase };
