01c80e3399ebc6bbe420dfedf7b03f90d757e7f5
enymaria923-prog <enymaria923@gmail.com>
2025-11-04 00:49:40 -0300

    Fix import path for PublicarEvento.css

--- app/admin/bokunohero/page.js
+++ app/admin/bokunohero/page.js
@@ -3,7 +3,7 @@
 import SetorManager from '../../publicar-evento/components/SetorManager';
 import CategoriaSelector from '../../publicar-evento/components/CategoriaSelector';
 import SelecionarTaxa from '../../publicar-evento/components/SelecionarTaxa';
-import '../../../publicar-evento/PublicarEvento.css';
+import '../../publicar-evento/PublicarEvento.css';
 import { supabase } from '../../utils/supabase/client';
 
 export default function AdminPage() {
