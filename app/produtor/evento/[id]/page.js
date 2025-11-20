0e18e4a67933b100f05a79c2e432a175b47f2cd6
enymaria923-prog <enymaria923@gmail.com>
2025-11-20 20:35:25 -0300

    Update page.js

--- app/produtor/evento/[id]/page.js
+++ app/produtor/evento/[id]/page.js
@@ -1,7 +1,7 @@
 'use client';
 import { useState, useEffect } from 'react';
 import { useRouter, useParams } from 'next/navigation';
-import { createClient } from '../../../../../utils/supabase/client';
+import { createClient } from '../../../../utils/supabase/client';
 
 export default function EditarEventoAdmin() {
   const supabase = createClient();
