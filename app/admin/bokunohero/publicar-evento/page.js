582754eac280dfbb51610743616036e4cf5bd12e
enymaria923-prog <enymaria923@gmail.com>
2025-12-03 00:15:15 -0300

    Update page.js

--- app/admin/bokunohero/publicar-evento/page.js
+++ app/admin/bokunohero/publicar-evento/page.js
@@ -7,6 +7,7 @@ import CategoriaSelector from '../../../publicar-evento/components/CategoriaSele
 import '../../../publicar-evento/PublicarEvento.css';
 
 export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 const supabase = createClient();
 const PublicarEventoAdmin = () => {
   const router = useRouter();
