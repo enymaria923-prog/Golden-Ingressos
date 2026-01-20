49c120cd36896e1bc7cdd923856613f6035e4699
enymaria923-prog <enymaria923@gmail.com>
2026-01-20 00:29:11 -0300

    Update page.js

--- app/evento/[id]/page.js
+++ app/evento/[id]/page.js
@@ -8,7 +8,7 @@ import MapaAssentos from '../../../components/MapaAssentos';
 import { getTeatroConfig } from '../../../utils/teatros-config';
 
 // 👇 IMPORTAR O CSS AQUI
-import '../[id]/page.css';
+import './page.css';
 
 function ProdutorInfo({ produtorId }) {
   const supabase = createClient();
