993b0298a25079431548a2ad48383d4688900499
enymaria923-prog <enymaria923@gmail.com>
2026-01-23 23:21:52 -0300

    Update page.js

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -3,6 +3,7 @@
 
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import '../styles/checkout.css';
 import Link from 'next/link';
 
 export default function CheckoutPage() {
